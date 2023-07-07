83a1e87c30d2ff75740fc648d2e54a11b0e59f58
nguyenle20 <nguyen.lechan20@gmail.com>
2023-07-07 16:02:03 +0700

    fix frontend deploy 0.1

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -22,7 +22,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const isFriend = friends.find((friend) => friend._id === friendId);
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `https://mern-social-media-server-mm6d.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
