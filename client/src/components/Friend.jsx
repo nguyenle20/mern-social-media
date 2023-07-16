84c878371ab526db72dcd81dc13c96191ce3f248
nguyenle20 <nguyen.lechan20@gmail.com>
2023-07-16 18:43:56 +0700

    fix bug friend list not working on click cus server from another url

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -42,7 +42,6 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
         <Box
           onClick={() => {
             navigate(`/profile/${friendId}`);
-            navigate(0);
           }}
         >
           <Typography
